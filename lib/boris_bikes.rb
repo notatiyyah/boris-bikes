e1fdac472df86780b27b43aa10463c0a7195120c
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-08 14:50:01 +0000

    Add tests to raise errors

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -2,16 +2,27 @@ class DockingStation
 
   attr_reader :docked_bikes
 
-  def initialize
+  def initialize(num_of_bikes=0)
     @docked_bikes = []
+    @capacity = 1
+    num_of_bikes.times {@docked_bikes << Bike.new}
   end
 
   def release_bike
-    Bike.new
+    bike = @docked_bikes.pop
+    if bike.nil?
+      raise "There are no bikes to release"
+    else
+      return bike
+    end
   end
 
   def dock_bike(bike)
-    @docked_bikes.push(bike)
+    if @docked_bikes.length >= @capacity
+      raise "There is no space in this docking station"
+    else
+      @docked_bikes.push(bike)
+    end
   end
 
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -8,7 +8,7 @@ describe DockingStation do
   end
   
   it "can release a bike" do
-    docking_station = DockingStation.new
+    docking_station = DockingStation.new(1)
     bike = docking_station.release_bike
     expect(bike).to respond_to(:working?)
   end
@@ -25,6 +25,17 @@ describe DockingStation do
     bike = Bike.new
     docking_station.dock_bike(bike)
     expect(docking_station.docked_bikes.length).to eq 1
+  end
+
+  it "raises an error if asked to release a bike where there are none" do
+    docking_station = DockingStation.new
+    expect { docking_station.release_bike }.to raise_error("There are no bikes to release")
+  end
+
+  it "raises an error if asked to dock a bike where there is no space" do
+    docking_station = DockingStation.new(1)
+    bike = Bike.new
+    expect { docking_station.dock_bike(bike) }.to raise_error("There is no space in this docking station")
   end
 
 end
